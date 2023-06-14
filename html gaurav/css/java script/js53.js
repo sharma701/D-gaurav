030865bb5008ebf58df97ef0a9190e4e797032bc
gaurav kumar <gaurav.eeepatna@gmail.com>
2023-06-14 18:08:36 +0530

    initial commit

---
+++ html gaurav/css/java script/js53.js
@@ -0,0 +1,18 @@
+console.log("This is tutorial 53")
+let name1="gaurav";
+let nam2="saurav";
+let name3="ankit";
+let name4="rahul";
+let greetText="good Morning"
+//console.log(name + " is a good boy'")
+//console.log(name2 + " is a good boy'")
+//console.log(name3 + " is a good boy'")
+//console.log(name4 + " is a good boy'")
+function greet(play,greetext="greeting fom js"){
+    console.log(greetext + " " +play)
+    console.log(play + " is a good boy")
+}
+greet(name1,greetText)
+greet(nam2,greetText)
+greet(name3,greetText)
+greet(name4)
